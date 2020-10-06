91575488307124d9e40117ef26209eb261f596ba
ziggymoens <ziggy.moens@student.hogent.be>
2020-10-06 10:59:01 +0200

    DatoCMS added to recipe
    everything from recipe comes from DatoCMS with layout

--- pages/[recipe].js
+++ pages/[recipe].js
@@ -1,16 +1,133 @@
+import React from 'react';
 import { useRouter } from "next/router";
+import { request } from '../lib/datocms';
 import Header from "../components/header";
 
-export default function Recipe() {
+const RECIPE_QUERY = `
+query Recipe ($limit: IntType){
+    allRecipes(first: $limit) {
+      title
+      chef {
+        name
+        picture {
+          responsiveImage(imgixParams: {fit: crop, w: "300", h: "300", auto: format}) {
+            srcSet
+            webpSrcSet
+            sizes
+            src
+            width
+            height
+            aspectRatio
+            alt
+            title
+            base64
+          }
+        }
+      }
+      combinewith
+      createdAt
+      ingredients {
+        name
+        quantity
+        amount
+      }
+      picture {
+        responsiveImage(imgixParams: {auto: format, fit: crop, h: "300", w: "300"}) {
+          srcSet
+          webpSrcSet
+          sizes
+          src
+          width
+          height
+          aspectRatio
+          alt
+          title
+          base64
+        }
+      }
+      instructions{
+        step
+        instructiontext
+      }
+      slug
+    }
+  }
+`
+
+export async function getStaticPaths() {
+    const data = await request({
+        query: RECIPE_QUERY,
+        variables: { limit: 3 }
+    })
+
+    const paths = data.allRecipes.map(r => ({
+        params: { recipe: r.slug },
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps() {
+    const data = await request({
+        query: RECIPE_QUERY,
+        variables: { limit: 3 }
+    })
+
+    return {
+        props: {
+            data
+        }
+    }
+}
+
+export default function Home({ data, params }) {
     const router = useRouter();
     const recipe = router.query;
+    console.log(recipe)
 
+    const reci = data.allRecipes.find(function (r) {
+        if (r.slug === recipe.recipe) {
+            return r;
+        }
+    });
+
+    if (router.isFallback) {
+        return <div>Loading...</div>
+    }
     return (
         <div>
-            <Header/>
-            <h2>
-                The Recipe: {recipe.recipe} is made by Unknown
-            </h2>
+            <Header />
+            <div class="container">
+                <div class="jumbotron">
+                    <h1 class="display-4">{reci.title}</h1>
+                    <p class="lead">Made by: {reci.chef.name} <br></br>Best combine with: {reci.combinewith}</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 col-md-8 col-sm-10">
+                        <h4>ingredients</h4>
+                        {reci.ingredients.map(ingredient => (
+                            <p>{ingredient.amount} {ingredient.quantity} {ingredient.name}</p>
+                        ))}
+                        <br></br>
+                        <h4>Instructions</h4>
+                        {reci.instructions.map(instruction => (
+                            <div>
+                                <h5>{instruction.step}</h5>
+                                <p>{instruction.instructiontext}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-2">
+                        <h3>{reci.chef.name}</h3>
+                        <img class="w-75" src={reci.chef.picture.responsiveImage.src} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
